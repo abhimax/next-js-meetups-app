48827e60048da50d9fdaac5aceab74e125f6bb7e
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-01-03 01:52:52 +0530

    feat: add component and home page structure

--- pages/news/[newsID].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-function DetailPage() {
-  const router = useRouter();
-    return <h1>The Detail Page { router.query.newsID }</h1>
-  }
-  
-  export default DetailPage;
