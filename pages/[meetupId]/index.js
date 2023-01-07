64ddd33198ac61dd75b22913d708064363ac535d
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-01-07 21:22:06 +0530

    fix: add meetup detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function MeetupDetails() {
+  return (
+    <Fragment>
+      <img
+        src="https://worldstrides.com/wp-content/uploads/2015/07/iStock_000040849990_Large.jpg"
+        alt="fmu"
+      />
+      <h1>First Meetup</h1>
+      <address>New York City, Park Street, 012234</address>
+      <p>New York City has a population of 23.58 million (urban area)</p>
+    </Fragment>
+  );
+}
+export default MeetupDetails;
