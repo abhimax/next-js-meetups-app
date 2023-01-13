190613db505d75d8e773b1d7d5238f0041199dd1
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-01-13 20:19:42 +0530

    feat: handle mongodb in the api route

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,18 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup <- api url
-function handler(req, res){
+async function handler(req, res){
     if(req.method === 'POST'){
         const data = req.body;
         const { title, image, address, description } = data;
         // store them in DB
+        const client = await MongoClient.connect('mongodb+srv://admin:<password>@clusterabhi.bpxffpl.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+        client.close();
+        res.status(201).json({ message: 'Meetup inserted!'});
     }
 }
 export default handler;
